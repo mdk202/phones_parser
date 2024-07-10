cb811b79f40397524683c0319a627b04a700abcf
mdk202 <94633975+mdk202@users.noreply.github.com>
2024-07-10 16:40:02 +0300

    Update ozon_crawler.py

--- parser_ozon/spiders/ozon_crawler.py
+++ parser_ozon/spiders/ozon_crawler.py
@@ -1,9 +1,6 @@
 import scrapy
 from scrapy.loader import ItemLoader
 
-# import sys
-# sys.path.append(r'C:\Users\malof\PycharmProjects\task_3_4\parser_ozon\parser_ozon')
-# from items import ParserOzonItem
 from ..items import ParserOzonItem
 
 
@@ -23,18 +20,18 @@ class SmartCrawlerSpider(scrapy.Spider):
                 clear_link = product.xpath('@href').get().split("?")[0]
                 self.smartphone_links.append(response.urljoin(clear_link))
         print(len(self.smartphone_links))
-        if len(self.smartphone_links) < 4:
+        if len(self.smartphone_links) < 100:
             self.page_counter += 1
             yield scrapy.Request(
                 url=response.urljoin(f'{self.next_page}{self.page_counter}'),
                 callback=self.parse
             )
-        # else:
-        #     for link in self.smartphone_links[0:2]:
-        #         yield scrapy.Request(
-        #             url=f'{link}features/',
-        #             callback=self.parse_detail, dont_filter=True
-        #         )
+        else:
+            for link in self.smartphone_links:
+                yield scrapy.Request(
+                    url=f'{link}features/',
+                    callback=self.parse_detail, dont_filter=True
+                )
 
     def parse_detail(self, response):
         loader = ItemLoader(item=ParserOzonItem(), response=response)
